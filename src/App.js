c1c19f47c6ba552151121c7a3a867b7a19eaa5dc
Anirban <anirbanmukherjee602@gmail.com>
2021-11-21 00:06:09 +0530

    states move to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,75 @@
 import './App.css';
+import React, { Component } from 'react';
 import CounterCard from './components/CounterCard';
 
-function App() {
-  return (
-    <div className="App">
-      <CounterCard/>
-      <CounterCard/>
-      <CounterCard/>
-    </div>
-  );
+// function Apps() {
+//   return (
+//     <div className="App">
+//       <CounterCard/>
+//       <CounterCard/>
+//       <CounterCard/>
+//     </div>
+//   );
+// }
+
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      items: [
+        {id: 1, value: 0},
+        {id: 2, value: 0},
+        {id: 3, value: 12}
+      ]
+    }
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+  }
+  onIncrement(id, value){
+    const newItemsArray = [...this.state.items];
+    newItemsArray[id-1].value = value+1;
+    this.setState({
+        items: newItemsArray
+    });
+  }
+  onDecrement(id, value){
+      const newItemsArray = [...this.state.items];
+      newItemsArray[id-1].value = value-1;
+      if(newItemsArray[id-1].value >= 0)
+      {
+        this.setState({
+            items: newItemsArray
+        });
+      }
+      else{
+          alert('Decrement Not Possible');
+      }
+  }
+  onDelete(id){
+    const newItemsArray = [...this.state.items];
+    newItemsArray[id-1].value = 0;
+    this.setState({
+      items: newItemsArray
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.items.map((item, i)=>(
+          <CounterCard 
+            id={this.state.items[i].id}
+            value={this.state.items[i].value}
+            onIncrement = {this.onIncrement}
+            onDecrement = {this.onDecrement}
+            onDelete = {this.onDelete}
+          />
+        ))}
+      </div>
+    )
+  }
 }
 
-export default App;
+
+// export default App;
--- src/components/CounterCard.jsx
+++ src/components/CounterCard.jsx
@@ -1,54 +1,54 @@
 import React, { Component } from 'react';
 import AddIcon from '@material-ui/icons/Add';
-import DeleteIcon from '@material-ui/icons/Delete';
+import RotateLeftIcon from '@material-ui/icons/RotateLeft';
 import Button from '@material-ui/core/Button';
 import RemoveIcon from '@material-ui/icons/Remove';
 
 
 export default class CounterCard extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            value: 0,
-        }
-        this.onIncrement = this.onIncrement.bind(this);
-        this.onDecrement = this.onDecrement.bind(this);
-        this.onDelete = this.onDelete.bind(this);
-    }
-    onIncrement(){
-        this.setState(prevState => ({
-            value: prevState.value + 1
-        }));
-    }
-    onDecrement(){
-        if(this.state.value > 0)
-        {
-            this.setState(this.setState(prevState => ({
-                value: prevState.value - 1
-            })))
-        }
-        else{
-            alert('Decrement Not Possible');
-        }
-    }
-    onDelete(){
-        this.setState({value : 0});
-    }
+    // constructor(props){
+    //     super(props);
+    //     this.state={
+    //         value: 0,
+    //     }
+    //     this.onIncrement = this.onIncrement.bind(this);
+    //     this.onDecrement = this.onDecrement.bind(this);
+    //     this.onDelete = this.onDelete.bind(this);
+    // }
+    // onIncrement(){
+    //     this.setState(prevState => ({
+    //         value: prevprops.value + 1
+    //     }));
+    // }
+    // onDecrement(){
+    //     if(this.props.value > 0)
+    //     {
+    //         this.setState(this.setState(prevState => ({
+    //             value: prevprops.value - 1
+    //         })))
+    //     }
+    //     else{
+    //         alert('Decrement Not Possible');
+    //     }
+    // }
+    // onDelete(){
+    //     this.setState({value : 0});
+    // }
     render() {
         return (
-            <div className="counter-card">
+            <div className="counter-card" key={this.props.id}>
                 <div className="counter-card-title">
-                    <font>{this.state.value}</font>
+                    <font style={{userSelect: 'none'}}>{this.props.value}</font>
                 </div>
                 <div className="counter-card-ft-btns">
-                    <Button color="primary" className="btn" onClick={this.onIncrement}>
+                    <Button color="primary" className="btn" onClick={()=>this.props.onIncrement(this.props.id,this.props.value)}>
                         <AddIcon fontSize="large" />
                     </Button>
-                    <Button color="primary" className="btn" disabled={this.state.value===0?true:false} onClick={this.onDecrement}>
+                    <Button color="primary" className="btn" disabled={this.props.value===0?true:false} onClick={()=>this.props.onDecrement(this.props.id,this.props.value)}>
                         <RemoveIcon fontSize="large"/>
                     </Button>
-                    <Button color="secondary" className="btn" onClick={this.onDelete}>
-                        <DeleteIcon fontSize="large"/>
+                    <Button color="secondary" className="btn" onClick={()=>this.props.onDelete(this.props.id,this.props.value)}>
+                        <RotateLeftIcon fontSize="large"/>
                     </Button>
                 </div>
             </div>
